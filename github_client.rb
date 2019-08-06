6a87c5a55f9240dafe6beb337106b2f1694fa145
kolen <incredible.angst@gmail.com>
2019-08-06 03:32:36 +0300

    Add github client (WIP)

---
+++ github_client.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "faraday"
+require "faraday_middleware"
+
+class GithubAPIClient
+  include Logging
+
+  def initialize(repo:, username:, token:)
+    @repo = repo
+    @username = username
+    @token = token
+  end
+
+  def pull_request_open?(id)
+    logger.debug "Fetching pull requests status #{@repo}##{id}"
+    resp = connection.get "pulls/#{id}"
+    logger.debug "PR #{@repo}##{id} is #{resp.body['state']}"
+    resp.body["state"] == "open"
+  end
+
+  def create_pull_request(hash, branch)
+    connection.post "pulls",
+                    title: "Bump CI docker image to #{hash[7..14]}",
+                    head: branch,
+                    base: "master",
+                    maintainer_can_modify: true,
+                    body: pull_request_body(hash)
+  end
+
+  private
+
+  def pull_request_body(hash)
+    <<~END
+      Update main docker image on CircleCI to `#{hash}`.
+
+      This is automated pull request.
+    END
+  end
+
+  def connection
+    @connection ||= Faraday.new "https://api.github.com/repos/#{@repo}/" do |conn|
+      conn.basic_auth @username, @token
+      conn.request :json
+      conn.response :raise_error
+      conn.response :json
+      conn.adapter :net_http
+    end
+  end
+end
