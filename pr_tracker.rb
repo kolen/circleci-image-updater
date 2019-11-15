d14b3ee945e1f80792657a0ff6bbbd2b56172239
kolen <incredible.angst@gmail.com>
2019-11-15 22:47:01 +0300

    Update PRTracker

--- pr_tracker.rb
+++ pr_tracker.rb
@@ -4,19 +4,31 @@ require "json"
 
 class PRTracker
   def initialize
-    @data = JSON.parse(File.open(filename))
+    if File.exists? filename
+      @data = JSON.parse(File.open(filename))
+    else
+      @data = {}
+    end
   end
 
   def save
-    JSON.dump(@data, File.open(filename, "w"))
+    File.open(filename, "w") do |f|
+      f.write(JSON.pretty_generate(@data))
+    end
   end
 
   def pr_for_repo(repo_id)
     @data[repo_id]
   end
 
+  def set_pr_for_repo(repo_id, pr_number)
+    @data[repo_id] = pr_number
+    save
+  end
+
   def remove_pr(repo_id)
     @data.delete(repo_id)
+    save
   end
 
   def filename
